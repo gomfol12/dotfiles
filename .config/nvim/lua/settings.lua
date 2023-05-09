6d2d8f4cb64070b751b599064ec862dc45f7741b
gomfol12 <marek@am-hohenstein.de>
2023-05-09 19:29:51 +0200

    fixed delay for xset and vim-pandoc

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -145,7 +145,37 @@ vim.g.undotree_WindowLayout = 3
 vim.g.goyo_width = 100
 
 -- vim-pandoc
-vim.cmd([[let g:pandoc#command#custom_open = 'zathura.sh']])
+vim.g["pandoc#command#custom_open"] = "MyPandocOpen"
+
+vim.cmd([[
+function! MyPandocOpen(file)
+    let file = shellescape(fnamemodify(a:file, ':p'))
+    let file_extension = fnamemodify(a:file, ':e')
+    if file_extension is? 'pdf'
+        if !empty($PDFVIEWER)
+            return expand('$PDFVIEWER') . ' ' . file
+        elseif executable('zathura')
+            return 'zathura ' . file
+        elseif executable('mupdf')
+            return 'mupdf ' . file
+        endif
+    elseif file_extension is? 'html'
+        if !empty($BROWSER)
+            return expand('$BROWSER') . ' ' . file
+        elseif executable('firefox')
+            return 'firefox ' . file
+        elseif executable('chromium')
+            return 'chromium ' . file
+        endif
+    elseif file_extension is? 'odt' && executable('okular')
+        return 'okular ' . file
+    elseif file_extension is? 'epub' && executable('okular')
+        return 'okular ' . file
+    else
+        return 'xdg-open ' . file
+    endif
+endfunction
+]])
 
 -- ========== Autocmds ========== --
 local augroup = vim.api.nvim_create_augroup
