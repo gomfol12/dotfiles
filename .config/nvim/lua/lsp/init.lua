1f32ed37572913724d407e36f983c1518c995043
gomfol12 <marek@am-hohenstein.de>
2023-07-08 14:15:39 +0200

    added rust language server

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -130,6 +130,7 @@ return setmetatable(_M, {
         lsp_config.svlangserver.setup(vim.tbl_deep_extend("force", require("lsp.settings.svlangserver_lua"), opts))
         lsp_config.texlab.setup(opts)
         lsp_config.pyright.setup(opts)
+        lsp_config.rust_analyzer.setup(vim.tbl_deep_extend("force", require("lsp.settings.rust_analyser_lua"), opts))
 
         require("clangd_extensions").setup({
             server = {
