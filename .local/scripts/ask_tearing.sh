4ba3099ebaea67eaef97b9ee4b741900936bcfed
gomfol12 <marek@am-hohenstein.de>
2025-03-18 14:52:02 +0100

    updated ask_tearing.sh

--- .local/scripts/ask_tearing.sh
+++ .local/scripts/ask_tearing.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-monitor="HDMI-A-1"
+monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .name')
 tearing=$(hyprctl monitors -j | jq -r '.[] | select(.name == "'"$monitor"'").activelyTearing')
 
 notify-send "Tearing" "Monitor $monitor tearing status: $tearing"
