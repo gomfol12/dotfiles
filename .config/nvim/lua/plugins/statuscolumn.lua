ab652107929bce1b73913fc199cdb26dd1808d65
gomfol12 <marek@am-hohenstein.de>
2025-04-07 13:56:02 +0200

    hyprland changed keyboard layout, nvim lint fortitude now in plugin and commented out statuscolumn

--- .config/nvim/lua/plugins/statuscolumn.lua
+++ .config/nvim/lua/plugins/statuscolumn.lua
@@ -1,33 +1,35 @@
 -- ==================== Statuscolumn (statuscol.nvim) ==================== --
 -- NOTE: get names `:I vim.fn.sign_getdefined()`
 
-return {
-    "luukvbaal/statuscol.nvim",
-    config = function()
-        local builtin = require("statuscol.builtin")
-        require("statuscol").setup({
+return {}
 
-            relculright = true,
-            segments = {
-                {
-                    sign = { namespace = { "gitsigns" }, colwidth = 1, wrap = true },
-                    click = "v:lua.ScLa",
-                },
-                {
-                    sign = { namespace = { "diagnostic/signs" }, colwidth = 2 },
-                    click = "v:lua.ScLa",
-                },
-                {
-                    sign = { name = { "Dap" }, colwidth = 1 },
-                    click = "v:lua.ScLa",
-                },
-                {
-                    text = { builtin.lnumfunc },
-                    condition = { true, builtin.not_empty },
-                    click = "v:lua.ScLa",
-                },
-                { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-            },
-        })
-    end,
-}
+-- return {
+--     "luukvbaal/statuscol.nvim",
+--     config = function()
+--         local builtin = require("statuscol.builtin")
+--         require("statuscol").setup({
+
+--             relculright = true,
+--             segments = {
+--                 {
+--                     sign = { namespace = { "gitsigns" }, colwidth = 1, wrap = true },
+--                     click = "v:lua.ScLa",
+--                 },
+--                 {
+--                     sign = { namespace = { "diagnostic/signs" }, colwidth = 2 },
+--                     click = "v:lua.ScLa",
+--                 },
+--                 {
+--                     sign = { name = { "Dap" }, colwidth = 1 },
+--                     click = "v:lua.ScLa",
+--                 },
+--                 {
+--                     text = { builtin.lnumfunc },
+--                     condition = { true, builtin.not_empty },
+--                     click = "v:lua.ScLa",
+--                 },
+--                 { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+--             },
+--         })
+--     end,
+-- }
