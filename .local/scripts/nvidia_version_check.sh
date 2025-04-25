c6931a3eb063ae1046e9562199ec82c8694a6479
gomfol12 <marek@am-hohenstein.de>
2025-04-25 18:15:13 +0200

    fix nvidia_version_check.sh

--- .local/scripts/nvidia_version_check.sh
+++ .local/scripts/nvidia_version_check.sh
@@ -2,7 +2,7 @@
 
 SYSTEM_VERSION=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader | tr -d ' ')
 
-FLATPAK_VERSION=$(flatpak list --columns=name | grep -oP 'nvidia-\K[0-9]+-[0-9]+-[0-9]+' | head -n 1 | tr '-' '.')
+FLATPAK_VERSION=$(flatpak list --columns=name | grep -oP 'nvidia-\K[0-9.-]+' | head -n 1 | tr '-' '.')
 
 if ! [[ "$SYSTEM_VERSION" == "$FLATPAK_VERSION" ]]; then
     notify-send "Flatpak / NVIDIA Driver Version Check" "NVIDIA versions differ! System: $SYSTEM_VERSION, Flatpak: $FLATPAK_VERSION"
