df3262d8111d5633662bfffed95f50cfca22f100
gomfol12 <info@marekb.de>
2025-08-27 18:11:06 +0200

    lf opener open object files with nvim

--- .local/scripts/lf/opener.sh
+++ .local/scripts/lf/opener.sh
@@ -92,7 +92,7 @@ case "$f" in
     ;;
 *)
     case $(file --mime-type "$f" -bL) in
-    text/* | application/json | inode/x-empty | application/octet-stream | application/x-sega-pico-rom | application/javascript | application/mbox)
+    text/* | application/json | inode/x-empty | application/octet-stream | application/x-sega-pico-rom | application/javascript | application/mbox | application/x-object)
         printf "%s" "$fx" | xargs -ro "$EDITOR"
         ;;
     image/x-xcf)
