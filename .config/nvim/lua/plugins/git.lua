4e4f3f2d6a55fc2e2644696fe97ebc3ecc022a41
gomfol12 <info@marekb.de>
2026-05-31 10:24:15 +0200

    nvim: lazygit plugin

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,6 +1,23 @@
 -- ==================== Gitsigns.nvim ==================== --
 
 return {
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        },
+    },
     "lewis6991/gitsigns.nvim",
     opts = {
         current_line_blame = true,
