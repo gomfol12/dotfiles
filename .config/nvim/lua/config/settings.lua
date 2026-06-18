fae448cc151b90a676f9642b35380abf1e6ec06c
gomfol12 <info@marekb.de>
2026-06-18 15:46:59 +0200

    nvim disabled modelines

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -35,6 +35,7 @@ vim.opt.listchars = "tab:>-,trail:-,precedes:<,extends:>,nbsp:+"
 vim.opt.conceallevel = 1
 vim.opt.pumheight = 10 -- Max number of items to show in the popup menu
 -- vim.opt.winborder = "single" -- Square borders
+vim.opt.modeline = false
 
 -- folding
 vim.opt.foldcolumn = "1"
